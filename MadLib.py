9f742b1456996925bb56131a782dfb1c59e76b5f
Alexis Indick <aindick@oswego.edu>
2020-12-23 12:09:50 -0500

    I input some silly code.

---
+++ MadLib.py
@@ -0,0 +1,9 @@
+#Mad Lib Generator#
+noun = input("Enter a noun: ")
+name = input("Enter a name: ")
+verb = input("Enter a verb: ")
+
+print("I once had a/an " + noun)
+print("Its name was " + name)
+print("But one day, it " + verb)
+print("It was a great " + noun)
